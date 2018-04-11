40751e98eb785721fa51c039650e046e4ef4b77a
谭浩 <tanhao@cdyoue.com>
2018-04-11 14:28:10 +0800

    first commit

---
+++ cdyoue-sydb-common/src/main/java/com/cdyoue/oddJobs/annotion/authentication/Role.java
@@ -0,0 +1,13 @@
+package com.cdyoue.oddJobs.annotion.authentication;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by liaoyoule on 2017/5/18.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface Role {
+    int value();
+}
